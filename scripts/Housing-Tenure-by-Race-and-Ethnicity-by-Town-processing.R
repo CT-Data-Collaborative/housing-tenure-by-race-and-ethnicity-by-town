8ece69e0c5abcf84791b2b5774fce22a31dc9bea
ilyankou <ilyankou@gmail.com>
2020-02-03 10:44:54 +0000

    Update ACS 2018 5y

--- scripts/Housing-Tenure-by-Race-and-Ethnicity-by-Town-processing.R
+++ scripts/Housing-Tenure-by-Race-and-Ethnicity-by-Town-processing.R
@@ -14,7 +14,7 @@ source('./scripts/acsHelpers.R')
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2010:2017)
+yearlist=c(2010:2018)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -421,7 +421,7 @@ tenure_data <- tenure_data %>%
 
 write.table (
   tenure_data,
-  file.path(getwd(), "data", "housing_tenure_race_town_2017.csv"),
+  file.path(getwd(), "data", "housing_tenure_race_town_2018.csv"),
   sep = ",",
   row.names = F,
   na = "-9999"
